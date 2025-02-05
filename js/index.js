b8be099baf695ef620e45023a8bff4864a142d69
Teresa <teresa@Teresas-MBP.hitronhub.home>
2025-02-05 13:06:28 -0400

    Added a button to change a quote on my site, created my js index and worked on my styling for the button (forgot to hit save previously

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ let quoteArray = [
     'I am who I say I am, not who you want me to be',
     'I am imperfectly perfect',
     'Beauty is always in the eye of the beholder',
-    'Treat others how you wouldmwant tobe treated',
+    'Treat others how you would want to be treated',
     'The quickest way to get somewhere is one step at a time',
     'It always seems impossible until it is done',
     "Every failure is a win, because if you've learned from your failure, it has now become a lesson"
