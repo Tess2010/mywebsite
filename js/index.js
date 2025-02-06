20cddc253a612e613a3790fababc1dd08487eac3
Teresa <teresa@Teresas-MBP.hitronhub.home>
2025-02-06 12:35:58 -0400

    Created the html, js and css to cycle through photos, remember to  add the other pictures to the array

--- js/index.js
+++ js/index.js
@@ -8,14 +8,29 @@ let quoteArray = [
     'It always seems impossible until it is done',
     "Every failure is a win, because if you've learned from your failure, it has now become a lesson"
 ]
-function changeText(){
+function changeText() {
     let quote = document.getElementById("quote");
-    let ran = Math.floor(Math.random()*6)
-    quote.innerText= quoteArray[ran];
+    let ran = Math.floor(Math.random() * 6)
+    quote.innerText = quoteArray[ran];
 }
 
 let currentIndex = 0;
-function moveImage (direction){
-    let prev =
-    let next = 
+function moveImage(direction) {
+    const arrPic = ["../images/Destinations/Italy-Bologna.jpg", 
+        "../images/Destinations/Mina_Port.jpg", "../images/Destinations/ArticFox.jpg" ];
+        
+    let image = document.getElementById("mainPic");
+    if (currentIndex + direction >= 0 && currentIndex + direction <= (arrPic.length - 1)) {
+            currentIndex += direction
+            image.src = arrPic[currentIndex];
+        }
+    else {
+        if (currentIndex == arrPic.length - 1) {
+            currentIndex = 0
+            image.src = arrPic[currentIndex]
+        } else {
+        currentIndex = arrPic.length - 1
+        image.src = arrPic[currentIndex]
+    }
+    }
 }
